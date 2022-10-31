2f382b8dcfd7d6a9a0cfa423252b384ef6d81300
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-10-31 17:28:35 +0300

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "white", "calm", "1"); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "white", "calm", "1"); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "black", "calm", "1"); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 2, "M", "brown", "calm", "1"); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, "F", "white", "calm", "1"); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "F", "green", "calm", "1"); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "F", "white", "calm", "1"); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "white", "calm", "1");
